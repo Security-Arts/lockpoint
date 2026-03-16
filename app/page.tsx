f65af66ffe5a4c97d7645623a30abf2bc255aa4c
Security-Arts <71318020+Security-Arts@users.noreply.github.com>
2026-03-16 22:12:11 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -471,8 +471,6 @@ const [lockedR, completedR, droppedR] = await Promise.all([
                   </Link>
                 </div>
 
-              </div>
-
               {/* RIGHT: form */}
               <div>
                 <div className="border border-zinc-200 dark:border-white/10 bg-zinc-50/50 dark:bg-white/3 p-6">
